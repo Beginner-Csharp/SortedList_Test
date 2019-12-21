4f144457c74dc13ef8cea15dbf8a4088bdd0ad7c
Newz CJ <newz.cj@gmail.com>
2019-12-22 03:54:26 +0700

    addAll

---
+++ SortedList_Test/SortedList_Test/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortedList_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();
+          
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            sortedList.Add(keyTxt.Text,valueTxt.Text);
+
+            ICollection collection = sortedList.Keys;
+
+            string data = "";
+            foreach(string key in collection)
+            {
+               data = data +  (sortedList[key].ToString());
+            }
+            MessageBox.Show(data);
+            keyTxt.Text = "";
+            valueTxt.Text = "";
+        }
+    }
+}
